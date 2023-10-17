d06b434a3136fbbe3374772b4491c3bda0dc148d
GeneralFinny <143330234+GeneralFinny@users.noreply.github.com>
2023-10-17 22:41:05 +0800

    Update script.js

--- script.js
+++ script.js
@@ -277,7 +277,7 @@ document.addEventListener('DOMContentLoaded', function () {
         placeholderMessage = displayBotPlaceholderMessage();
     
         // Perform the translation fetch
-        fetch("https://deb9-136-158-26-7.ngrok-free.app/translate", {
+        fetch("http://127.0.0.1:5000/translate", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json"
